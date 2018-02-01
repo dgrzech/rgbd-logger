02c1fbda608d496d4e95eed7c595794f437b3433
daniel grzech <dgrzech17@imperial.ac.uk>
2018-02-01 22:13:12 +0000

    src/rs-log.cpp: Add Print out camera intrinsics

--- src/rs-log.cpp
+++ src/rs-log.cpp
@@ -8,9 +8,9 @@
 /* opencv includes */
 #include <opencv2/opencv.hpp>
 
-#include <fstream>  // File IO
-#include <iostream> // Terminal IO
-#include <sstream>  // Stringstreams
+#include <fstream>  /* file io */
+#include <iostream> /* terminal io */
+#include <sstream>  /* stringstreams */
 
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
@@ -48,9 +48,10 @@ void setup_viewers() {
 /* helper function for writing metadata to disk as a .csv file */
 void metadata_to_csv(const rs2::frame &frm, const std::string &filename);
 
-// this method captures frames, displays them, and writes them to disk
 int main(int argc, char *argv[]) try {
-  // build output dirs
+  rs2::context rsContext;
+
+  /* build output dirs */
   fs::path framesDir(filePath);
   fs::create_directory(framesDir);
   fs::path colorDir(filePath + "/color");
@@ -58,28 +59,45 @@ int main(int argc, char *argv[]) try {
   fs::path depthDir(filePath + "/depth");
   fs::create_directory(depthDir);
 
-  // declare realsense pipeline, encapsulating the actual device and sensors
+  /* declare realsense pipeline, encapsulating the actual device and sensors */
   rs2::pipeline pipe;
 
-  // create a configuration for the pipeline with a custom profile
+  /* create a configuration for the pipeline with a custom profile */
   rs2::config cfg;
 
-  // add desired streams to configuration
+  /* add colour and depth streams to configuration */
   cfg.enable_stream(RS2_STREAM_COLOR, WIDTH, HEIGHT, RS2_FORMAT_BGR8,
                     FRAME_RATE);
   cfg.enable_stream(RS2_STREAM_DEPTH, WIDTH, HEIGHT, RS2_FORMAT_Z16,
                     FRAME_RATE);
 
-  // start streaming with my config
-  pipe.start(cfg);
+  /* start pipeline */
+  rs2::pipeline_profile selection = pipe.start(cfg);
 
-  // camera warmup--dropping several first frames to let auto-exposure
-  // stabilize
+  /* get depth stream and print depth camera intrinsics */
+  auto depth_stream =
+      selection.get_stream(RS2_STREAM_DEPTH).as<rs2::video_stream_profile>();
+
+  auto resolution = std::make_pair(depth_stream.width(), depth_stream.height());
+  auto i = depth_stream.get_intrinsics();
+  auto principal_point = std::make_pair(i.ppx, i.ppy);
+  auto focal_length = std::make_pair(i.fx, i.fy);
+  rs2_distortion model = i.model;
+
+  std::cout << "depth camera principal point         : "
+            << principal_point.first << ", " << principal_point.second
+            << std::endl;
+  std::cout << "depth camera focal length            : " << focal_length.first
+            << ", " << focal_length.second << std::endl;
+
+  /* camera warmup--drop several first frames to let
+   auto-exposure stabilize */
   rs2::frameset frames;
   for (int i = 0; i < 30; i++) {
     frames = pipe.wait_for_frames();
   }
 
+  /* set up viewers */
   setup_viewers();
 
   int frameCounter = 0;
@@ -87,18 +105,17 @@ int main(int argc, char *argv[]) try {
   while (true) {
     frames = pipe.wait_for_frames();
 
-    // get each frame
+    /* get each frame */
     rs2::frame color_frame = frames.get_color_frame();
     rs2::frame depth_frame = frames.get_depth_frame();
 
-    // create opencv mat from a color image
+    /* create opencv mat from the color image */
     cv::Mat color(cv::Size(WIDTH, HEIGHT), CV_8UC3,
                   (void *)color_frame.get_data(), cv::Mat::AUTO_STEP);
-    // create opencv mat from depth image
+    /* create opencv mat from the depth image */
     cv::Mat depth(cv::Size(WIDTH, HEIGHT), CV_16UC1,
                   (void *)depth_frame.get_data(), cv::Mat::AUTO_STEP);
 
-    /* increment the frame counter */
     std::stringstream ss;
     ss << std::setw(6) << std::setfill('0') << frameCounter;
     std::string frameNum = ss.str();
@@ -108,9 +125,10 @@ int main(int argc, char *argv[]) try {
     cv::imwrite("frames/color/" + fileName + "-" + frameNum + ".color.png",
                 color);
 
+    /* increment the frame counter */
     frameCounter++;
 
-    // display the images in a gui
+    /* display the images in a gui */
     imshow(WINDOW_COLOR, color);
     cvWaitKey(1);
 
@@ -122,7 +140,7 @@ int main(int argc, char *argv[]) try {
 
   return 0;
 } catch (const rs2::error &e) {
-  std::cerr << "realSense error calling " << e.get_failed_function() << "("
+  std::cerr << "realsense error calling " << e.get_failed_function() << "("
             << e.get_failed_args() << "):\n    " << e.what() << std::endl;
   return EXIT_FAILURE;
 } catch (const std::exception &e) {
@@ -135,11 +153,11 @@ void metadata_to_csv(const rs2::frame &frm, const std::string &filename) {
 
   csv.open(filename);
 
-  // std::cout << "Writing metadata to " << filename << endl;
+  std::cout << "writing metadata to " << filename << std::endl;
   csv << "stream," << rs2_stream_to_string(frm.get_profile().stream_type())
       << "\nMetadata Attribute,Value\n";
 
-  // Record all the available metadata attributes
+  /* record all the available metadata attributes */
   for (size_t i = 0; i < RS2_FRAME_METADATA_COUNT; i++) {
     if (frm.supports_frame_metadata((rs2_frame_metadata_value)i)) {
       csv << rs2_frame_metadata_to_string((rs2_frame_metadata_value)i) << ","
